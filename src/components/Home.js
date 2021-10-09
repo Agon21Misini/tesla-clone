471c44668bbb8bb2eeb4d041516ad420ea3fb3ab
Agon21Misini <misiniagon80@gmail.com>
2021-10-09 14:48:37 +0200

    ADD: redux for cars

--- src/components/Home.js
+++ src/components/Home.js
@@ -3,61 +3,65 @@ import styled from 'styled-components'
 import Section from './Section'
 
 function Home() {
-    return (
-        <Container>
-            <Section 
-                title="Model S"
-                description="Order Online for Touchless Delivery"
-                backgroundImg="model-s.jpg"
-                leftBtnText="Custom order"
-                rightBtnText="Existing inventory"
-            />
-            <Section 
-                title="Model Y"
-                description="Order Online for Touchless Delivery"
-                backgroundImg="model-y.jpg"
-                leftBtnText="Custom order"
-                rightBtnText="Existing inventory"
-            />
-            <Section 
-                title="Model 3"
-                description="Order Online for Touchless Delivery"
-                backgroundImg="model-3.jpg"
-                leftBtnText="Custom order"
-                rightBtnText="Existing inventory"
-            />
-            <Section 
-                title="Model X"
-                description="Order Online for Touchless Delivery"
-                backgroundImg="model-x.jpg"
-                leftBtnText="Custom order"
-                rightBtnText="Existing inventory"
-            />
-            <Section 
-                title="Solar Panels"
-                description="Lowest Cost Solar Panels in America"
-                backgroundImg="solar-panel.jpg"
-                leftBtnText="Order now"
-                rightBtnText="Learn more"
-            />
-            <Section 
-                title="Solar Roof"
-                description="Produce Clean Energy From Your Roof"
-                backgroundImg="solar-roof.jpg"
-                leftBtnText="Order now"
-                rightBtnText="Learn more"
-            />
-            <Section 
-                title="Accessories"
-                backgroundImg="accessories.jpg"
-                leftBtnText="Shop now"
-            />
-        </Container>
-    )
+	return (
+		<Container>
+			<Section 
+				title="Model S" 
+				description="Order Online fro Touchless Delivery"
+				backgroundImg="model-s.jpg"
+				leftBtnText="Custom order"
+				rightBtnText="Exisiting inventory"
+			/>
+			<Section 
+				title="Model 3" 
+				description="Order Online fro Touchless Delivery"
+				backgroundImg="model-3.jpg"
+				leftBtnText="Custom order"
+				rightBtnText="Exisiting inventory"
+			/>
+			<Section 
+				title="Model X" 
+				description="Order Online fro Touchless Delivery"
+				backgroundImg="model-x.jpg"
+				leftBtnText="Custom order"
+				rightBtnText="Exisiting inventory"
+			/>
+			<Section 
+				title="Model Y" 
+				description="Order Online fro Touchless Delivery"
+				backgroundImg="model-y.jpg"
+				leftBtnText="Custom order"
+				rightBtnText="Exisiting inventory"
+			/>
+			<Section
+				title="Lowerst Cost Solar Panels in America"
+				description="Money-back guarantee"
+				backgroundImg="solar-panel.jpg"
+				leftBtnText="Oder now"
+				rightBtnText="Learn more"
+			>
+			</Section>
+			<Section
+				title="Solar for New Roofs"
+				description="Solar Roof Costs Less Than a New"
+				backgroundImg="solar-roof.jpg"
+				leftBtnText="Oder now"
+				rightBtnText="Learn more"
+			>
+			</Section>
+			<Section
+				title="Accessories"
+				description=""
+				backgroundImg="accessories.jpg"
+				leftBtnText="Shop now"
+			>
+			</Section>
+		</Container>
+	)
 }
 
 export default Home
 
-const Container = styled.div`
-    height: 100vh;
+const Container = styled.div `
+	height: 100vh;	
 `
